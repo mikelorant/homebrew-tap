3b3f3ea462d1ec4699e46635661506f4486d119d
Michael Lorant <michael.lorant@fairfaxmedia.com.au>
2020-06-26 13:00:33 +1000

    Lint BIG IP edge client

--- Casks/big-ip-edge-client.rb
+++ Casks/big-ip-edge-client.rb
@@ -2,6 +2,7 @@ cask 'big-ip-edge-client' do
   version '7.1.9'
   sha256 '44eadfe89a38da6099e2cbdbca0a882cf5932bd5ff02f431235533fe2bffa0e1'
 
+  # ffxblue-infrastructure-vpn.s3-ap-southeast-2.amazonaws.com was verified as official when first introduced to the cask
   url 'https://ffxblue-infrastructure-vpn.s3-ap-southeast-2.amazonaws.com/mac_edgesvpn.pkg'
   name 'BIG IP Edge Client'
   homepage 'https://techdocs.f5.com/kb/en-us/bigip-edge-apps.html'
@@ -9,7 +10,7 @@ cask 'big-ip-edge-client' do
   pkg 'mac_edgesvpn.pkg'
 
   uninstall pkgutil: [
-    'F5EdgeClient',
-    'F5SSLVpnPlugin'
-  ]
+                       'F5EdgeClient',
+                       'F5SSLVpnPlugin',
+                     ]
 end
