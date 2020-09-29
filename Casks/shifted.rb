49ae28c661ce95d30639160929eb5743a1956b76
Michael Lorant <michael.lorant@fairfaxmedia.com.au>
2020-09-29 16:37:16 +1000

    Add Shifted cask

---
+++ Casks/shifted.rb
@@ -0,0 +1,13 @@
+cask "shifted" do
+  version "18"
+  sha256 "82e7da90bd80c4d2e79a6dc1e6c9399ad8aaf91f99b4ee04264c05a74270306c"
+
+  # dl.devmate.com/com.techbutler.Shifted/ was verified as official when first introduced to the cask
+  url "https://dl.devmate.com/com.techbutler.Shifted/Shifted.dmg"
+  appcast "https://updates.devmate.com/com.techbutler.Shifted.xml"
+  name "Shifted"
+  desc "Specify a custom action that will be triggered when you hit the caps lock key"
+  homepage "https://getshifted.app/"
+
+  app "Shifted.app"
+end
